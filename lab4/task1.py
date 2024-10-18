93f012249be5ec19d9920d3dcf2aa7091928a581
Ivan Shtarev <ishtarev@outlook.com>
2024-10-18 20:45:57 +0300

    lab4 done

---
+++ lab4/task1.py
@@ -0,0 +1,10 @@
+import json
+
+def task() -> float:
+    with open('input.json', 'r') as file:
+        data = json.load(file)
+    
+    total = sum(item['score'] * item['weight'] for item in data)
+    return round(total, 3)
+
+print(task())
